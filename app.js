301c82151d4b6077a0a7902e378c6de3bc048005
Joe Santos <jaysantos012@gmail.com>
2021-06-11 19:48:25 -0400

    finished all Css, now working on the JS for functionlaity

---
+++ app.js
@@ -0,0 +1,14 @@
+const cardsContainerEL = document.getElementById('cards-container');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const currentEL = document.getElementById('current');
+const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
+const questionEL = document.getElementById('question');
+const answerEL = document.getElementById('answer');
+const addCardBtn = document.getElementById('add-card');
+const clearBtn = document.getElementById('clear');
+const addContainer = document.getElementById('add-container');
+
+
+
