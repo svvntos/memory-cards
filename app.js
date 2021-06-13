b25c487e72c520bb489c8fa993a55353074cae1f
Joe Santos <jaysantos012@gmail.com>
2021-06-12 23:42:12 -0400

    added the click ir touch even where the card flips. now working the JS to add the new card action

--- app.js
+++ app.js
@@ -12,3 +12,56 @@ const addContainer = document.getElementById('add-container');
 
 
 
+let currentActiveCard = 0;
+
+const cardEl = [];
+
+const cardsData = [
+  {
+    question: 'What must a variable begin with?',
+    answer: 'A letter, $ or _'
+  },
+  {
+    question: 'What is a variable?',
+    answer: 'Container for a piece of data'
+  },
+  {
+    question: 'Example of Case Sensitive Variable',
+    answer: 'thisIsAVariable'
+  }
+];
+
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+}
+
+function createCard(data, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if(index === 0){
+        card.classList.add('active');
+    }
+
+    card.innerHTML = `
+    <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+    `;
+
+    card.addEventListener('click' || 'touchstart', () => card.classList.toggle('show-answer'));
+
+    cardEl.push(card);
+    cardsContainerEL.appendChild(card);
+}
+
+createCards();
